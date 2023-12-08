6d67bff14872cc2c7c236e98d6bb96a171b00d23
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2023-12-07 21:21:42 -0600

    edited the apis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
-// need to add this locationInput id to html <input type="text" id="locationInput" placeholder="Enter Location">
-// <button onclick="searchRestaurants()">Search</button>
 function searchRestaurants() {
     var location = document.getElementById('locationInput').value;
-  
     var urlRest = `https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchLocation?query=${location}`;
-  
     var optionsRest = {
       method: 'GET',
       headers: {
@@ -13,58 +9,46 @@ function searchRestaurants() {
       }
     };
   
-    // Perform the API request using the modified URL and options
-    // ...
-    new Promise((resolve, reject) => {
-        fetch(urlRest, optionsRest)
-          .then(response => response.text())
-          .then(result => {
-            resolve(result);
-          })
-          .catch(error => {
-            reject(error);
-          });
-      })
-        .then(result => {
-          console.log(result);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-  }
 
-
-// need to add this zipCOdeInput id to the html <input type="text" id="zipCodeInput" placeholder="Enter Zip Code">
-// <button onclick="searchHotels()">Search</button
-function searchHotels() {
- var zipCode = document.getElementById('zipCodeInput').value;
-      
-var url = `https://hotels4.p.rapidapi.com/locations/v3/search?q=${zipCode}&locale=en_US&langid=1033&siteid=300000001`;
-      
-var options = {
- method: 'GET',
-     headers: {
-            'X-RapidAPI-Key': '2483fe3bcdmsh626c8ea7487af6ap1137bdjsn316ab5a9048f',
-            'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
-          }
-        };
-      
-        // Perform the API request using the modified URL and options
-        // ...
-        new Promise((resolve, reject) => {
-            fetch(url, options)
-              .then(response => response.text())
-              .then(result => {
-                resolve(result);
-              })
-              .catch(error => {
-                reject(error);
-              });
-          })
-            .then(result => {
-              console.log(result);
-            })
-            .catch(error => {
-              console.error(error);
-            });
+    var urlHotels = `https://hotels4.p.rapidapi.com/locations/v3/search?q=${location}&locale=en_US&langid=1033&siteid=300000001`;
+    var optionsHotels = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'YOUR_HOTELS_API_KEY',
+        'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
       }
+    };
+  
+    // Perform the API request to retrieve restaurant data
+    fetch(urlRest, optionsRest)
+      .then(response => response.json())
+      .then(data => {
+        // Display the first three restaurant results
+        var restaurants = data.results.slice(0, 3);
+        restaurants.forEach(restaurant => {
+          // Display the restaurant information on the page
+          // ...
+        });
+      })
+      .catch(error => {
+        // Handle any errors that occur during the restaurant API request
+        console.log(error);
+      });
+  
+    // Perform the API request to retrieve hotel data
+    fetch(urlHotels, optionsHotels)
+      .then(response => response.json())
+      .then(data => {
+        // Display the first three hotel results
+        var hotels = data.suggestions[0].entities.slice(0, 3);
+        hotels.forEach(hotel => {
+          // Display the hotel information on the page
+          // ...
+        });
+      })
+      .catch(error => {
+        // Handle any errors that occur during the hotel API request
+        console.log(error);
+      });
+  }
+      
